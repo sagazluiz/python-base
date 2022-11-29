ba6c715f0fd5d0d4aff825e9e94e9c3901b668d6
sagazluiz <sagazluiz@hotmail.com>
2022-11-28 21:35:16 -0300

    done

---
+++ interpolacao.py
@@ -0,0 +1,12 @@
+for cliente in clientes:
+    ...:     print(
+    ...:     email_tmpl
+    ...:     % {
+    ...:     "nome": cliente,
+    ...:     "produto": "caneta",
+    ...:     "texto": "Escreve macio",
+    ...:     "link": "https://canetaslegais.com",
+    ...:     "quantidade": 1,
+    ...:     "preço": 50.5,
+    ...:         }
+    ...:         )
---
+++ tabuada.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+"""Imprime a tabuada do 1 ao 10
+
+---Tabuada do 1---
+    1 x 1 = 1
+    2 x 1 = 2
+    3 x 1 = 3
+...
+##########
+-------------------
+
+---Tabuada do 2---
+    2 x 1 = 2
+    2 x 2 = 4
+...
+###############
+
+"""
+
+__version__ = "0.1.1"
+__author__ = "Bruno"
+
+# numeros = [1,2,3,4,5,6,7,8,9]
+template = """
+
+--- Tabuada do 2 ---
+
+{bloco:^18}
+
+    
+######################
+
+""" 
+    
+numeros = list(range(1, 11))
+
+# Iterable (percorriveis)
+for n1 in numeros:
+    print("{:-^18}".format("Tabuada do 2"))
+    print()
+    for n2 in numeros:
+        print(n2)
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} X {n2} = {resultado}"))
+        #rint(operacao)
+    print("#" * 18)
