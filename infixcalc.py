a724813a2b0891a41c6aa2b12a8803fe99b9406b
sagazluiz <sagazluiz@hotmail.com>
2023-01-17 22:19:38 -0300

    Código para calculadora infix.

---
+++ infixcalc.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""Calculadora infix.
+
+
+Funcionamento:
+
+[operacao] [n1] [n2]
+
+operações:
+
+sum -> +
+sub -> -
+mul -> *
+div -> /
+
+Uso:
+$ infixcalc.py sum 5 2 
+7
+
+$ infixcalc.py mul 10 5
+50
+
+infixcalc.py 
+operacao: sum
+n1: 5
+n2: 4
+9
+
+
+"""
+
+__version__ = "0.0.1"
+
+import sys
+
+arguments = sys.argv[1:]
+
+if not arguments:
+	operation = input("operacao:")
+	n1 = input("n1:")
+	n2 = input("n2:")
+	arguments = [operation, n1, n2]
+elif len(arguments) != 3:
+	print("Número de argumentos inválidos")
+	print("soma 5 5")
+	sys.exit(1)	
+
+operation, *nums = arguments
+
+valid_operations = ("sum", "sub", "mul", "div")
+if operation not in valid_operations:
+	print("Operação inválida")
+	print(valid_operations)
+	sys.exit()
+
+validated_nums = []
+for num in nums:
+	if not num.replace(".", "").isdigit():
+		print(f"Número inválido {num}")
+		sys.exit(1)
+	if "." in num:
+		num = float(num)
+	else:
+		num = int(num)
+	validated_nums.append(num)
+
+n1, n2 = validated_nums
+
+if operation =="sum":
+	result = n1 + n2
+elif operation == "sub":
+	result = n1 - n2
+elif operation == "mul":
+	result = n1 * n2
+elif operation == "div":
+	result == n1 / n2
+
+print(f"O resultado é {result}")
+
+
