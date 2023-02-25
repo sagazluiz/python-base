2ce69cec8625ffe0119215e4c65b4a0b34def601
sagazluiz <sagazluiz@hotmail.com>
2023-02-24 21:10:47 -0300

    Error: EAFP, try, except

--- errors.py
+++ errors.py
@@ -2,19 +2,27 @@
 import os
 import sys
 
-# LBYL - Look Before You Leap
+# EAFP - Easy to ASK Forgiveness than Permission
 
-if os.path.exists("names.txt"):
-    print("O arquivo existe")
-    input("...")    # Race Condition
+try:
+    raise RuntimeError("An error ocurred duing the execution time")
 
-    names = open("names.txt").readlines()
-else:
-    print("[Error] File not found")
+try:
+    names = open("names.txt").readlines() # FileNotFoundError
+    1 / 1 # ZeroDivisionError
+    print(names.append) # AttributeError
+except FileNotFoundError as e: # Bare Except
+    print(f"[Error] {str(e)}")
     sys.exit(1)
-    
-if len(names) >= 3:
+    # retry -- pode ser feito com função recursiva ou while
+except ZeroDivisionError:
+    print("You can not divide per zero")
+    sys.exit(1)
+except AttributeError:
+    print("List does not has banana")
+    sys.exit(1)    
+try: 
     print(names[2])
-else:
+except:
     print("[Error] Missing name in the list")
     sys.exit(1)
