ee4985c9b6ad06884f30a7aed8b9dee2bcc889a3
sagazluiz <sagazluiz@hotmail.com>
2023-03-02 11:29:44 -0300

    Utilizando função para validar dados de input

--- alertas.py
+++ alertas.py
@@ -17,39 +17,55 @@ umidade: 90
 "ALERTA!!! 🥵♒ Perigo de calor úmido"
 """
 
-import sys
-import logging
+import logging 
+
 log = logging.Logger("alerta")
 
-info = {
-    "temperatura": None, 
-    "umidade": None
-}
-keys = info.keys()
 
-for key in keys:
-    try:
-        info[key] = float(input(f"Qual a {key}?").strip())
-    except ValueError:
-        log.error(f"{key.captalize()} invalida")
-        sys.exit(1)
+# TODO: Mover para módulo de utilidades. 
 
-temp = info["temperatura"]
-umidade = info["umidade"]
+def is_completely_filled(dict_of_inputs):
+    """ Returns a boolean telling if a dict is completelely filled."""
+    info_size = len(dict_of_inputs)
+    filled_size = len(
+        [value for value in dict_of_inputs.values() if value is not None]
+    )
+    return info_size == filled_size
+
+
+def read_inputs_for_dict(dict_of_info):
+    """ Reads information for a dict from user input """
+    for key in info.keys(): # ["temperatura", "umidade"]
+        if info[key] is not None:
+            continue
+        try:
+            dict_of_info[key] = int(input(f"{key}:").strip())
+        except ValueError:
+            log.error("$s inválida, digite núemros", key)
+            break # para for
+
+# PROGRAMA PRINCIPAL
+
+info = {"temperatura": None, "umidade": None}
+
+while not is_completely_filled(info):
+    read_inputs_for_dict(info)
+
+temp, umidade = info.values() # unpacking [30, 90]
 
 if temp > 45:
-    print("Alerta!!! Perigo calor extremo")
-elif temp * 3 >= umidade:
-    print("Alerta!!! Perigo de calor umido")
+    print("ALERTA!!! 🥵 Perigo calor extremo")
+elif temp > 30 and temp * 3 >= umidade:
+    print("ALERTA!!! 🥵♒ Perigo de calor úmido")
 elif temp >= 10 and temp <= 30:
-    print("Normal")
+    # elif 10 <= temp <= 30:
+    # elif temp in range(1, 31):
+    print("😀 Normal")
 elif temp >= 0 and temp <= 10:
-    print("Frio")
+    print("🥶 Frio")
 elif temp < 0:
-    print("Alerta Frio Extremo")
-
-
-
+    print("ALERTA!!! ⛄ Frio Extremo.")
+            
 
 
         
